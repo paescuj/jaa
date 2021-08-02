02f61b889ad6ac97b31aac93a4a1c74ab1ea1fee
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-08-02 15:45:08 +0200

    Fix error with new compose v. & enhance run script

--- run.sh
+++ run.sh
@@ -2,23 +2,34 @@
 set -e
 set -o pipefail
 
+# Global vars
 _dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 _jaa_env=${JAA_ENV:-dev}
 _stack_name='jaa'
-_docker_compose_cmd=('docker-compose' '--env-file' "${_dir}/.env.${_jaa_env}" '--file' "${_dir}/docker-compose.yml" '--file' "${_dir}/docker-compose.${_jaa_env}.yml")
-readonly _dir _jaa_env _stack_name _docker_compose_cmd
-
-if [[ $_jaa_env != 'dev' && $_jaa_env != 'prod' ]]; then
-  echo "JAA_ENV must either be 'dev' or 'prod'"
-  exit 1
+_env_file="${_dir}/.env.${_jaa_env}"
+_compose_files=("${_dir}/docker-compose.yml" "${_dir}/docker-compose.${_jaa_env}.yml")
+if docker compose >/dev/null 2>&1; then
+  _docker_compose_cmd=('docker' 'compose')
+else
+  _docker_compose_cmd=('docker-compose')
 fi
+_docker_compose_cmd+=('--env-file' "$_env_file")
+for compose_file in "${_compose_files[@]}"; do
+  _docker_compose_cmd+=('--file' "$compose_file")
+done
+_get_env_cmd=('sed' '-e' '/^#/d;/^\s*$/d' '-e' "s/'/'\\\''/g" '-e' "s/=\(.*\)/='\1'/g" "$_env_file")
+_secrets=(
+  'DIRECTUS_KEY'
+  'DIRECTUS_SECRET'
+  'DIRECTUS_DB_PASSWORD'
+  'DIRECTUS_ADMIN_PASSWORD'
+  'PAPERCUPS_KEY'
+  'PAPERCUPS_DB_PASSWORD'
+  'PAPERCUPS_SMTP_PASSWORD'
+)
+readonly _dir _jaa_env _stack_name _env_file _compose_files _docker_compose_cmd _get_env_cmd _secrets
 
-if [[ -z $DOCKER_HOST ]] && [[ -f "${_dir}/.env.${_jaa_env}" ]]; then
-  set -a
-  source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" "${_dir}/.env.${_jaa_env}" | grep '^DOCKER_HOST=')
-  set +a
-fi
-
+# Function to create env file / secrets
 init() {
   getUuid() {
     {
@@ -48,100 +59,104 @@ init() {
     } 2>/dev/null
   }
 
-  public_domain=${PUBLIC_DOMAIN:-example.org}
-  papercups_key=${PAPERCUPS_KEY:-$(getPassword 64)}
-  papercups_db_password=${PAPERCUPS_DB_PASSWORD:-$(getPassword 24)}
-  papercups_registration_disabled=${PAPERCUPS_REGISTRATION_DISABLED:-false}
-  directus_key=${DIRECTUS_KEY:-$(getUuid)}
-  directus_secret=${DIRECTUS_SECRET:-$(getUuid)}
-  directus_db_password=${DIRECTUS_DB_PASSWORD:-$(getPassword 24)}
-  directus_admin_password=${DIRECTUS_ADMIN_PASSWORD:-$(getPassword 12)}
+  local public_domain="${PUBLIC_DOMAIN:-jaa.example.org}"
+  declare -A config=(
+    [PUBLIC_DOMAIN]="$public_domain"
+    [DIRECTUS_KEY]="${DIRECTUS_KEY:-$(getUuid)}"
+    [DIRECTUS_SECRET]="${DIRECTUS_SECRET:-$(getUuid)}"
+    [DIRECTUS_DB_PASSWORD]="${DIRECTUS_DB_PASSWORD:-$(getPassword 24)}"
+    [DIRECTUS_ADMIN_PASSWORD]="${DIRECTUS_ADMIN_PASSWORD:-$(getPassword 12)}"
+    [PAPERCUPS_KEY]="${PAPERCUPS_KEY:-$(getPassword 64)}"
+    [PAPERCUPS_DB_PASSWORD]="${PAPERCUPS_DB_PASSWORD:-$(getPassword 24)}"
+    [PAPERCUPS_REGISTRATION_DISABLED]="${PAPERCUPS_REGISTRATION_DISABLED:-false}"
+    [DOCKER_HOST]="$DOCKER_HOST"
+    [REGISTRY_PREFIX]="${REGISTRY_PREFIX:-}"
+    [TRAEFIK_NETWORK]="${TRAEFIK_NETWORK:-web}"
+    [TRAEFIK_ENTRYPOINTS]="${TRAEFIK_ENTRYPOINTS:-websecure}"
+    [TRAEFIK_CERTRESOLVER]="${TRAEFIK_CERTRESOLVER:-le}"
+    [PAPERCUPS_SMTP_HOST]="${PAPERCUPS_SMTP_HOST:-mail.${public_domain}}"
+    [PAPERCUPS_SMTP_PORT]="${PAPERCUPS_SMTP_PORT:-465}"
+    [PAPERCUPS_SMTP_SSL]="${PAPERCUPS_SMTP_SSL:-true}"
+    [PAPERCUPS_SMTP_USER]=${PAPERCUPS_SMTP_USER:-papercups@${public_domain}}
+    [PAPERCUPS_SMTP_PASSWORD]="${PAPERCUPS_SMTP_PASSWORD:-$(getPassword 12)}"
+  )
 
-  declare -a envs
-  envs+=("PUBLIC_DOMAIN=${public_domain}")
-  envs+=("PAPERCUPS_REGISTRATION_DISABLED=${papercups_registration_disabled}")
+  declare -a envs=(
+    'PUBLIC_DOMAIN'
+    'PAPERCUPS_REGISTRATION_DISABLED'
+  )
 
   if [[ $_jaa_env == 'dev' ]]; then
-    web_url="http://localhost:3000"
-    directus_url="http://localhost:8055"
-    papercups_url="http://localhost:4000"
+    local web_url="http://localhost:3000"
+    local directus_url="http://localhost:8055"
+    local papercups_url="http://localhost:4000"
 
-    envs+=("PAPERCUPS_KEY=${papercups_key}")
-    envs+=("PAPERCUPS_DB_PASSWORD=${papercups_db_password}")
-    envs+=("DIRECTUS_KEY=${directus_key}")
-    envs+=("DIRECTUS_SECRET=${directus_secret}")
-    envs+=("DIRECTUS_DB_PASSWORD=${directus_db_password}")
-    envs+=("DIRECTUS_ADMIN_PASSWORD=${directus_admin_password}")
+    envs+=(
+      'DIRECTUS_KEY'
+      'DIRECTUS_SECRET'
+      'DIRECTUS_DB_PASSWORD'
+      'DIRECTUS_ADMIN_PASSWORD'
+      'PAPERCUPS_KEY'
+      'PAPERCUPS_DB_PASSWORD'
+    )
   else
-    web_url="https://${public_domain}"
-    directus_url="https://directus.${public_domain}"
-    papercups_url="https://papercups.${public_domain}"
+    local web_url="https://${public_domain}"
+    local directus_url="https://directus.${public_domain}"
+    local papercups_url="https://papercups.${public_domain}"
 
-    registry_prefix=${REGISTRY_PREFIX:-}
-    traefik_network=${TRAEFIK_NETWORK:-web}
-    traefik_entrypoints=${TRAEFIK_ENTRYPOINTS:-websecure}
-    traefik_certresolver=${TRAEFIK_CERTRESOLVER:-le}
-    papercups_smtp_host=${PAPERCUPS_SMTP_HOST:-mail.${public_domain}}
-    papercups_smtp_port=${PAPERCUPS_SMTP_PORT:-465}
-    papercups_smtp_ssl=${PAPERCUPS_SMTP_SSL:-true}
-    papercups_smtp_user=${PAPERCUPS_SMTP_USER:-papercups@${public_domain}}
-    papercups_smtp_password=${PAPERCUPS_SMTP_PASSWORD:-$(getPassword 12)}
-
-    envs+=("REGISTRY_PREFIX=${registry_prefix}")
-    envs+=("TRAEFIK_NETWORK=${traefik_network}")
-    envs+=("TRAEFIK_ENTRYPOINTS=${traefik_entrypoints}")
-    envs+=("TRAEFIK_CERTRESOLVER=${traefik_certresolver}")
-    envs+=("PAPERCUPS_SMTP_HOST=${papercups_smtp_host}")
-    envs+=("PAPERCUPS_SMTP_PORT=${papercups_smtp_port}")
-    envs+=("PAPERCUPS_SMTP_SSL=${papercups_smtp_ssl}")
-    envs+=("PAPERCUPS_SMTP_USER=${papercups_smtp_user}")
-    if [[ -n $DOCKER_HOST ]]; then
-      envs+=("DOCKER_HOST=${DOCKER_HOST}")
-    fi
+    envs+=(
+      'DOCKER_HOST'
+      'REGISTRY_PREFIX'
+      'TRAEFIK_NETWORK'
+      'TRAEFIK_ENTRYPOINTS'
+      'TRAEFIK_CERTRESOLVER'
+      'PAPERCUPS_SMTP_HOST'
+      'PAPERCUPS_SMTP_PORT'
+      'PAPERCUPS_SMTP_SSL'
+      'PAPERCUPS_SMTP_USER'
+    )
 
     echo 'Creating required Docker secrets...'
-    {
-      printf '%s' "$directus_key" | docker secret create jaa_directus_key -
-      printf '%s' "$directus_secret" | docker secret create jaa_directus_secret -
-      printf '%s' "$directus_db_password" | docker secret create jaa_directus_db_password -
-      printf '%s' "$directus_admin_password" | docker secret create jaa_directus_admin_password -
-      printf '%s' "$papercups_key" | docker secret create jaa_papercups_key -
-      printf '%s' "$papercups_db_password" | docker secret create jaa_papercups_db_password -
-      printf '%s' "$papercups_smtp_password" | docker secret create jaa_papercups_smtp_password -
-    } >/dev/null
+    for secret in "${_secrets[@]}"; do
+      printf '%s' "${config[$secret]}" | docker secret create jaa_${secret,,} - >/dev/null
+    done
   fi
 
-  if [[ -f "${_dir}/.env.${_jaa_env}" ]]; then
-    read -r -p "The file .env.${_jaa_env} already exists. Do you want to overwrite it? [y/N] " response
+  if [[ -f "$_env_file" ]]; then
+    read -r -p "The file $(basename "$_env_file") already exists. Do you want to overwrite it? [y/N] " response
     if [[ ! ${response,,} =~ ^y(es)?$ ]]; then
       exit
     fi
+    rm "$_env_file"
   fi
-  echo "Creating .env.${_jaa_env} file..."
-  printf "%s\n" "${envs[@]}" >"${_dir}/.env.${_jaa_env}"
+  echo "Creating $(basename "$_env_file") file..."
+  for env in "${envs[@]}"; do
+    printf '%s=%s\n' "$env" "${config[$env]}" >>"$_env_file"
+  done
 
   echo "Access data:
   Web:
     URL:  ${web_url}/admin
-    Code: ${directus_admin_password}
+    Code: ${config[DIRECTUS_ADMIN_PASSWORD]}
 
   Directus:
     URL:      ${directus_url}
     User:     admin@${public_domain}
-    Password: ${directus_admin_password}
+    Password: ${config[DIRECTUS_ADMIN_PASSWORD]}
 
   Papercups:
     URL: ${papercups_url}"
     if [[ $_jaa_env == 'prod' ]] && [[ -z $PAPERCUPS_SMTP_PASSWORD ]]; then
-      echo "    SMTP Password: ${papercups_smtp_password}"
+      echo "    SMTP Password: ${config[PAPERCUPS_SMTP_PASSWORD]}"
     fi
 }
 
+# Function to delete all data
 destroy() {
   read -r -p "All data will be deleted! Do you want to continue? [y/N] " response
   if [[ ${response,,} =~ ^y(es)?$ ]]; then
     set +e
-    touch "${_dir}/.env.${_jaa_env}"
+    touch "$_env_file"
     if [[ $_jaa_env == 'dev' ]]; then
       echo 'Removing containers...'
       "${_docker_compose_cmd[@]}" down --volumes
@@ -150,27 +165,24 @@ destroy() {
       docker stack rm jaa
       sleep 10
       echo 'Removing secrets...'
-      docker secret rm \
-        jaa_directus_key \
-        jaa_directus_secret \
-        jaa_directus_db_password \
-        jaa_directus_admin_password \
-        jaa_papercups_key \
-        jaa_papercups_db_password \
-        jaa_papercups_smtp_password \
-        >/dev/null
+      declare -a secrets
+      for secret in "${_secrets[@]}"; do
+        secrets+=("jaa_${secret,,}")
+      done
+      docker secret rm "${secrets[@]}" >/dev/null
       echo 'Removing volumes...'
       for volume in $("${_docker_compose_cmd[@]}" config --volumes 2>/dev/null); do
         docker volume rm "${_stack_name}_${volume}" >/dev/null
       done
     fi
     echo 'Removing env file...'
-    rm "${_dir}/.env.${_jaa_env}"
+    rm "$_env_file"
     set -e
   fi
   exit
 }
 
+# Function to build / upload images
 do_images() {
   local build_args=('build' '--pull' '--parallel')
   if [[ -n $1 ]]; then
@@ -184,7 +196,7 @@ do_images() {
     DOCKER_HOST="$DOCKER_BUILD_HOST" "${_docker_compose_cmd[@]}" "${build_args[@]}"
 
     echo 'Uploading images...'
-    source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" "${_dir}/.env.${_jaa_env}" | grep '^REGISTRY_PREFIX=')
+    source <("${_get_env_cmd[@]}" | grep '^REGISTRY_PREFIX=')
     if [[ -n $REGISTRY_PREFIX ]]; then
       DOCKER_HOST="$DOCKER_BUILD_HOST" "${_docker_compose_cmd[@]}" push
     else
@@ -194,23 +206,33 @@ do_images() {
   fi
 }
 
+# Function to start the project
 start() {
   if [[ $_jaa_env == 'dev' ]]; then
     echo 'Starting containers...'
     "${_docker_compose_cmd[@]}" up -d
 
     set -a
-    source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" "${_dir}/.env.${_jaa_env}")
+    source <("${_get_env_cmd[@]}")
     set +a
     echo "Starting web app..."
     npm install --prefix "${_dir}/web"
     exec npm run --prefix "${_dir}/web" dev
   else
     echo 'Deploying stack...'
-    exec docker stack deploy --compose-file <("${_docker_compose_cmd[@]}" config) "$_stack_name"
+    set -a
+    source <("${_get_env_cmd[@]}" | grep -v '^DOCKER_HOST=')
+    set +a
+    docker_stack_deploy_cmd=('docker' 'stack' 'deploy')
+    for compose_file in "${_compose_files[@]}"; do
+      docker_stack_deploy_cmd+=('--compose-file' "$compose_file")
+    done
+    docker_stack_deploy_cmd+=("$_stack_name")
+    exec "${docker_stack_deploy_cmd[@]}"
   fi
 }
 
+# Function to execute command in Docker context
 cmd() {
   if [[ $_jaa_env == 'dev' ]]; then
     exec "${_docker_compose_cmd[@]}" "$@"
@@ -219,24 +241,46 @@ cmd() {
   fi
 }
 
-case "$1" in
-'init')
-  init
-  if [[ $2 != '--no-start' ]]; then
-    do_images
-    start
+main() {
+  # Check env
+  if [[ $_jaa_env != 'dev' && $_jaa_env != 'prod' ]]; then
+    echo "JAA_ENV must either be 'dev' or 'prod'" >&2
+    exit 1
   fi
-  ;;
-'do-images')
-  do_images "$2"
-  ;;
-'destroy')
-  destroy
-  ;;
-'')
-  start
-  ;;
-*)
-  cmd "$@"
-  ;;
-esac
+
+  # Check env file / load DOCKER_HOST var
+  if [[ $1 != 'init' ]]; then
+    if [[ ! -f "$_env_file" ]]; then
+      echo "Missing file $(basename "$_env_file")"
+      exit 1
+    elif [[ -z $DOCKER_HOST ]]; then
+      set -a
+      source <("${_get_env_cmd[@]}" | grep '^DOCKER_HOST=')
+      set +a
+    fi
+  fi
+
+  case "$1" in
+  'init')
+    init
+    if [[ $2 != '--no-start' ]]; then
+      do_images
+      start
+    fi
+    ;;
+  'do-images')
+    do_images "$2"
+    ;;
+  'destroy')
+    destroy
+    ;;
+  '')
+    start
+    ;;
+  *)
+    cmd "$@"
+    ;;
+  esac
+}
+
+main "$@"

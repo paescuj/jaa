242269a819ee9d6888b4d731df46e3dd02f3eb7d
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-09-16 09:24:33 +0200

    Updates related to Chatwoot v1.20.0

--- web/components/Chat.js
+++ web/components/Chat.js
@@ -61,6 +61,8 @@ export default function Chat({ url, token, identifier, name, hash }) {
               var BASE_URL="${url}";
               var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
               g.src=BASE_URL+"/packs/js/sdk.js";
+              g.defer = true;
+              g.async = true;
               s.parentNode.insertBefore(g,s);
               g.onload=function(){
                 window.chatwootSDK.run({

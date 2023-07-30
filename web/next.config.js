89b65fc3f2c2c979d1d7b7c498b4179e1fa4252b
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-07-30 02:00:59 +0200

    Finalize new web dockerfile

--- web/next.config.js
+++ web/next.config.js
@@ -1,22 +1,25 @@
 module.exports = {
-  output: 'standalone',
-  webpack: (config) => {
-    // Load pdf worker files as URLs by using asset modules
-    config.module.rules.unshift({
-      test: /pdf\.worker\.(min\.)?js/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/worker/[hash][ext][query]',
-      },
-    });
+	output: 'standalone',
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	webpack: (config) => {
+		// Load pdf worker files as URLs by using asset modules
+		config.module.rules.unshift({
+			test: /pdf\.worker\.(min\.)?js/,
+			type: 'asset/resource',
+			generator: {
+				filename: 'static/worker/[hash][ext][query]',
+			},
+		});
 
-    // Workaround for issue "Module not found: Package path ./jsx-runtime.js is not exported from package"
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react/jsx-runtime.js': 'react/jsx-runtime',
-      'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
-    };
+		// Workaround for issue "Module not found: Package path ./jsx-runtime.js is not exported from package"
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'react/jsx-runtime.js': 'react/jsx-runtime',
+			'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
+		};
 
-    return config;
-  },
+		return config;
+	},
 };
